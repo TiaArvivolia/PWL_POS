0f8d50f7920f52d6b7ae065968e2b81de951f009
TiaArvivolia <arvivoliatia@gmail.com>
2024-09-20 10:03:06 +0700

    Jobsheet 4 - Praktikum 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -71,16 +71,16 @@ class UserController extends Controller
         //     ],
         // );
 
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user->save();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+        // return view('user', ['data' => $user]);
         
         // $user = UserModel::create(
         //     [
@@ -108,6 +108,28 @@ class UserController extends Controller
         // $user->isDirty(); // false
         // $user->isClean(); // true
         // dd($user->isDirty());
+
+        $user = UserModel::create(
+            [
+                'username' => 'manager11',
+                'nama' => 'Manager11',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+
+        $user->username = 'manager12';
+
+        $user->save();
+        
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username']));
+        
+        
+
         
     }
 }
