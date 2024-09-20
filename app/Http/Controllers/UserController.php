824a3de0d52d2bcc44cc3b8a4ade5e12eda6a4eb
TiaArvivolia <arvivoliatia@gmail.com>
2024-09-20 07:29:07 +0700

    Jobsheet 4 - Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index() 
     {
-        // tambah data user dengan Eloquent Model
+        // // tambah data user dengan Eloquent Model
         // $data = [
         //     'username' => 'customer-1',
         //     'nama' => 'Pelanggan',
@@ -19,10 +19,18 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data);   // tambahkan data ke tabel m_user
 
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
+        
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
         
         // coba akses model UserModel
         $user = UserModel::all();   // ambil semua data dari tabel m_user
