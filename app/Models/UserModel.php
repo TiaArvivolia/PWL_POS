69b0527fac2b2998716e1a0e42364f6b600c3116
TiaArvivolia <arvivoliatia@gmail.com>
2024-10-11 06:13:58 +0700

    JS 7 - Praktikum 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\LevelModel;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
 class UserModel extends Authenticatable
@@ -25,5 +27,18 @@ class UserModel extends Authenticatable
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function getRoleName(): string //Mendapatkan nama role
+    {
+        return $this->level->level_nama;
+    }
+    public function hasRole($role): bool // Cek apakah user memiliki role tertentu
+    {
+        return $this->level->level_kode == $role;
+    }
+    public function getRole()
+    {
+        return $this->level->level_kode;
     }
 }
